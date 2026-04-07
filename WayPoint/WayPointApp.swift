70bc605599bc20463bde9d7d35ec754c81aed849
Clyde Lartey <dclartey@outlook.com>
2026-04-07 15:53:13 -0400

    Initial Commit

---
+++ WayPoint/WayPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  WayPointApp.swift
+//  WayPoint
+//
+//  Created by Clyde Lartey on 07/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct WayPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
